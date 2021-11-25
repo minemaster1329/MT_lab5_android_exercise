c52cb15cbceff974ab883062d1920a7014b9e77b
minemaster1329 <minemaster1329@onet.com.pl>
2021-11-25 23:00:38 +0100

    bbb

--- app/src/main/java/com/example/mt_lab5_android_exercise/MyGLRenderer.kt
+++ app/src/main/java/com/example/mt_lab5_android_exercise/MyGLRenderer.kt
@@ -3,6 +3,8 @@ package com.example.mt_lab5_android_exercise
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import android.os.SystemClock
+import androidx.core.graphics.rotationMatrix
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -11,6 +13,8 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     private val vPMatrix = FloatArray(16)
     private val projectionMatrix = FloatArray(16)
     private val viewMatrix = FloatArray(16)
+    private val rotationMatrix = FloatArray(16)
+    private val translateMatrix = FloatArray(16)
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
@@ -27,15 +31,22 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     }
 
     override fun onDrawFrame(gl: GL10?) {
+        val scratch = FloatArray(16)
+
+        val time = SystemClock.uptimeMillis() % 4000L
+        val angle = 0.090f * time.toInt()
+
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
         triangle.draw()
+
 //        // Set the camera position (View matrix)
         Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
-//
 //        // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
-//
-//        // Draw shape
+
+        Matrix.rotateM(vPMatrix, 0, angle, 0f,0f,-1.0f)
+        Matrix.translateM(vPMatrix, 0, 0f, 0f, 0f)
+
         triangle.draw(vPMatrix)
     }
 }
